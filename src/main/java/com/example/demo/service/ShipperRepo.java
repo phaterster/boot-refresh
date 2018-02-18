00a635643d74dfaea450a36f966d487a451c4fd5
pmarecki <pmarecki.pro@gmail.com>
2018-02-18 11:41:55 +0100

    2nd DB test

--- src/main/java/com/example/demo/service/ShipperRepo.java
+++ src/main/java/com/example/demo/service/ShipperRepo.java
@@ -4,5 +4,8 @@ import com.example.demo.model.Shipper;
 import com.example.demo.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ShipperRepo extends CrudRepository<Shipper,Integer> {
+    Optional<Shipper> findByShippername(String shippername);
 }
